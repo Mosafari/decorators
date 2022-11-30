8ac82af58d456bcd7a9424007d7fea66698645e1
mosafari <m.safari2680@gmail.com>
2022-11-30 19:53:22 +0330

    wrapping output using decorators

---
+++ decorators.py
@@ -0,0 +1,16 @@
+# wrapping output using decorators
+
+def dec(func):
+    def owrapper():
+        print('_'*20)
+        func()
+        print('_'*20)
+    return owrapper
+    
+
+@dec
+def myprint(str="default"):
+    """Just prints Hi"""
+    print("Hi")
+    
+myprint()
